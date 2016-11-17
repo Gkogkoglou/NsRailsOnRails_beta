8b45262611d9c3316fb40c4ee264596d32a42a7f
Konstantinos <konstantinos.gkogkoglou@gmail.com>
2016-11-17 16:50:12 +0100

    submitting buttons and getting trains

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,45 +1,43 @@
 
 
 class ClientController < ApplicationController
-require 'active_support/core_ext/hash'
+  require 'active_support/core_ext/hash'
 
   def index
 
+    #def connect_to_client_and_get_stations
+     ns_client = Ns::Client.new('konstantinos.gkogkoglou@gmail.com', 'EpJBpuxVdTkDYV8Rsu0GNgbcnQwDo9RM9I8J7-gFMVZHVVH8XZVBqQ')
+     available_stations = ns_client.get_stations
+     @hash = Hash.from_xml(available_stations)
+     #get all codes and names
+     @root_node       = @hash["stations"]     || {}
+     @stations        = @root_node["station"] || []
+     @codes_and_names = @stations.compact.map do |station|
+             {
+               :code       => station["code"],
+               :name       => station['name']
+             }
+              end
+              #get all station names
+              @names = @stations.compact.map do |station|
+                station['name']
+                end
+
+              #get all station codes
+              @codes= @stations.compact.map do |station|
+                station["code"]
+              end
+    #end
+
+
+  end
+
+  def options
     ns_client = Ns::Client.new('konstantinos.gkogkoglou@gmail.com', 'EpJBpuxVdTkDYV8Rsu0GNgbcnQwDo9RM9I8J7-gFMVZHVVH8XZVBqQ')
+    @travel_advices = ns_client.get_travel_advice(params[:from], params[:to])
+    
 
-    #debugger
-    available_stations = ns_client.get_stations
-      #trip = Ns::Trip.new(from: 'Amsterdam Centraal', to: 'Ede Centrum', arrival: Time.now)
-
-
-      @hash = Hash.from_xml(available_stations)
-
-      #get all codes and names
-      @root_node       = @hash["stations"]     || {}
-      @stations        = @root_node["station"] || []
-      @codes_and_names = @stations.compact.map do |station|
-        {
-          :code       => station["code"],
-          :lang       => station['name']
-        }
-      end
-      #get all station names
-      @names = @stations.compact.map do |station|
-      station['name']
-      end
-
-      #get all station codes
-      @codes= @stations.compact.map do |station|
-          station["code"]
-      end
-
-      debugger
-
-
-
-
-        end
-
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   root to: 'pages#home'
+
+  get 'clients/options', :to => 'client#options'
+
   resources :client
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
