9f25237ff7251c7090a419758c13c0c5a52aa0d3
Konstantinos <konstantinos.gkogkoglou@gmail.com>
2016-11-17 11:59:11 +0100

    finally got all names and codes

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,14 +1,46 @@
 
 
 class ClientController < ApplicationController
+require 'active_support/core_ext/hash'
 
   def index
 
     ns_client = Ns::Client.new('konstantinos.gkogkoglou@gmail.com', 'EpJBpuxVdTkDYV8Rsu0GNgbcnQwDo9RM9I8J7-gFMVZHVVH8XZVBqQ')
 
+    #debugger
     available_stations = ns_client.get_stations
-    advice = ns_client.get_travel_advice(available_stations[0], available_stations[1])
-    end
+      #trip = Ns::Trip.new(from: 'Amsterdam Centraal', to: 'Ede Centrum', arrival: Time.now)
+
+
+      @hash = Hash.from_xml(available_stations)
+
+      #get all codes and names
+      @root_node       = @hash["stations"]     || {}
+      @stations        = @root_node["station"] || []
+      @codes_and_names = @stations.compact.map do |station|
+        {
+          :code       => station["code"],
+          :lang       => station['name']
+        }
+      end
+      #get all station names
+      @names = @stations.compact.map do |station|
+      station['name']
+      end
+
+      #get all station codes
+      @codes= @stations.compact.map do |station|
+          station["code"]
+      end
+
+      debugger
+
+
+
+
+        end
+
+
 
 
 end
